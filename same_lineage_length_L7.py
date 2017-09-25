efa8d2cc734e92a664e7f95bc6262f9568ff2e08
Pedro J. Torres <pjtorres88@gmail.com>
2017-09-25 15:05:46 -0700

    Update and rename head_L7_length.py to same_lineage_length_L7.py

--- same_lineage_length_L7.py
+++ same_lineage_length_L7.py
@@ -1,13 +1,13 @@
 # Allows you to make sure your taxonomic header is the same length throughout taxonomy. Sometimes you get outputs from Blast in which 
-#taxonomic lineage tps at class, but others at species, so this code will fill the rest of your taxonomy with the lowest taxonomic lineage
+#taxonomic lineage stops at class, but others at species, so this code will fill the rest of your taxonomy with the lowest taxonomic lineage
 #known until it reaches a length of 7 (aka species level).
 
 
-f= open("try2.txt").read().splitlines()
+f= open("inputfile.txt").read().splitlines()# input file
+fout=open('newheaders.txt','w+')#output file with new headers
 
-fout=open('newheaders.txt','w+')
 print 'We are starting'
-for line in f:# remeber to remove any commas you can use the substitute function in excel =SUBSTITUTE(A271,",","")
+for line in f:# remeber to remove any commas you have inbetween the ';' character which seperates your taxonomic lineage. You can use the substitute function in excel =SUBSTITUTE(A271,",","")
     line=line.split(';')
     linelen=len(line)
     while linelen < 7:
@@ -20,7 +20,7 @@ for line in f:# remeber to remove any commas you can use the substitute function
         final=str(line)+'\n'
         #print final
         fout.write(final)
-
+fin.close()
 fout.close()
 print 'Done'
 
