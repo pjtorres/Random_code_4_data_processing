7f46ed73a9bd602d82ff8c643c73101bfba2c361
Pedro J. Torres <pjtorres88@gmail.com>
2017-09-25 08:26:46 -0700

    Update head_L7_length.py

--- head_L7_length.py
+++ head_L7_length.py
@@ -2,14 +2,15 @@
 #taxonomic lineage tps at class, but others at species, so this code will fill the rest of your taxonomy with the lowest taxonomic lineage
 #known until it reaches a length of 7 (aka species level).
 
+
 f= open("try2.txt").read().splitlines()
 
 fout=open('newheaders.txt','w+')
-for line in f:  # remeber to remove any commas you can use the substitute function in excel!
+print 'We are starting'
+for line in f:# remeber to remove any commas you can use the substitute function in excel =SUBSTITUTE(A271,",","")
     line=line.split(';')
     linelen=len(line)
-#    print line
-    while linelen != 7:
+    while linelen < 7:
         nle=len(line)
         name=line[nle-1]
         line.append(name)
@@ -17,8 +18,9 @@ for line in f:  # remeber to remove any commas you can use the substitute functi
     else:
         line=("\t".join(line))
         final=str(line)+'\n'
-        print final
+        #print final
         fout.write(final)
 
 fout.close()
-print "Done yo :)"
+print 'Done'
+
