eb271bc309644ab65650bfbbadaff4c7a2c9b8cc
Pedro J. Torres <pjtorres88@gmail.com>
2018-08-21 12:59:02 -0700

    Update get_taxid.sh

--- get_taxid.sh
+++ get_taxid.sh
@@ -8,5 +8,7 @@ echo "$1"
 
 grep "$1"  taxonomy/names.dmp | cut -d'|' -f1| sort | uniq
 
-# example
+#example
 #./get_taxid.sh 'Alistipes shahii'
+#group analyis 
+# while read line; do ./get_taxid.sh "${line%}"; done <test.txt
