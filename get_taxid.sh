cca6d9780f78c19698e341839c759376dbb51101
Pedro J. Torres <pjtorres88@gmail.com>
2018-08-21 12:21:15 -0700

    Create get_taxid.sh

---
+++ get_taxid.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#collect seqids for taxa of interest
+
+taxa=$1
+
+echo "$1" 
+
+grep "$1"  taxonomy/names.dmp | cut -d'|' -f1| sort | uniq
+
+# example
+#./get_taxid.sh 'Alistipes shahii'
